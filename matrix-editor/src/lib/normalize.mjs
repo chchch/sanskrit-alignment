11367dc81f8c4cd746c21c84f9a9ebb6e4d10994
chchch <charles.li@cnrs.fr>
2025-04-13 10:23:47 +0200

    added ṣk/ḥk sandhi

--- matrix-editor/src/lib/normalize.mjs
+++ matrix-editor/src/lib/normalize.mjs
@@ -168,7 +168,7 @@ const filters_slpish = [
     },
     {
         name: 'internal visarga variants',
-        search: 'ṣ(?=[kK])|s(?=s)',
+        search: '[ṣḥ](?=[kK])|s(?=s)',
         replace: () => 'ḥ'
     },
     {
